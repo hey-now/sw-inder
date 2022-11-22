751323f1d84457ff90842a553012f0e33e130bdc
John Britton <johnbritton@Enterprise-5.local>
2022-11-22 10:11:49 -0700

    First Push

---
+++ main_app/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+
+# Create your views here.
+
+def home(request):
+  return render(request, 'home.html')
+
+def about(request):
+  return render(request, 'main_app/about.html')
+
+@login_required
+def interests(request):
+  return render(request, 'main_app/interests.html')
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('/')
+    else:
+      error_message = 'Invalid signup - try again'
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'registration/signup.html', context)  
